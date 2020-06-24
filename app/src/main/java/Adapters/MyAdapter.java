4fb616d185540fc19a7d677c7f4a8c639843c1d9
Cath12 <noodle256215@gmail.com>
2020-06-24 18:50:29 -0500

    Lista mejorada

---
+++ app/src/main/java/Adapters/MyAdapter.java
@@ -0,0 +1,49 @@
+package Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.pc2.R;
+
+
+public class MyAdapter extends ArrayAdapter<String> {
+    Context context;
+    String rTitle[];
+    String rPedido[];
+    String rEntrega[];
+    int rImgs[];
+    public MyAdapter(Context c, String title[], String pedido[], String entrega[], int imgs[]){
+        super(c, R.layout.row , R.id.textView1,title);
+        this.context=c;
+        this.rTitle=title;
+        this.rPedido= pedido;
+        this.rEntrega=entrega;
+        this.rImgs=imgs;
+    }
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater layoutInflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View row = layoutInflater.inflate(R.layout.row,parent,false);
+        ImageView images = row.findViewById(R.id.image);
+        TextView myTitle = row.findViewById(R.id.textView1);
+        TextView myPedido = row.findViewById(R.id.textView2);
+        TextView myEntrega = row.findViewById(R.id.textView3);
+
+        images.setImageResource(rImgs[position]);
+        myTitle.setText(rTitle[position]);
+        myPedido.setText(rPedido[position]);
+        myEntrega.setText(rEntrega[position]);
+
+        return row;
+    }
+
+}
--- app/src/main/java/com/example/pc2/lista.java
+++ app/src/main/java/com/example/pc2/lista.java
@@ -3,8 +3,17 @@ package com.example.pc2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class lista extends AppCompatActivity {
+
+    ListView listView;
+    String mTitle[] = {"Los Rodríguez", "Santoro", "La Palomino", "Che carlitos", "Tradición arequipeña"};
+    String mPedido[] ={"Pedido mínimo: S/.13","Pedido mínimo: S/.13", "Pedido mínimo: S/.16",
+            "Pedido mínimo: S/.25", "Pedido mínimo: S/.15"};
+    String mEntrega[]={"Entrega: De 45 a 60 minutos", "Entrega: De 45 a 60 minutos", "Entrega: De 45 a 60 minutos"
+            "Entrega: De 45 a 60 minutos", "Entrega: De 45 a 60 minutos"};
+    int images[]= {R.drawable.i1, R.drawable.i2, R.drawable.i3, R.drawable.i4, R.drawable.i5};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
