b24d9b8723741665fee348be934c2fe34a7871d6
Cath12 <noodle256215@gmail.com>
2020-06-24 22:50:27 -0500

    Añadiendo mapa

--- app/src/main/java/Adapters/MyAdapter.java
+++ app/src/main/java/Adapters/MyAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -20,6 +21,7 @@ public class MyAdapter extends ArrayAdapter<String> {
     String rPedido[];
     String rEntrega[];
     int rImgs[];
+    RatingBar rb;
     public MyAdapter(Context c, String title[], String pedido[], String entrega[], int imgs[]){
         super(c, R.layout.row , R.id.textView1,title);
         this.context=c;
@@ -37,6 +39,7 @@ public class MyAdapter extends ArrayAdapter<String> {
         TextView myTitle = row.findViewById(R.id.textView1);
         TextView myPedido = row.findViewById(R.id.textView2);
         TextView myEntrega = row.findViewById(R.id.textView3);
+        rb= row.findViewById(R.id.ratingBar);
 
         images.setImageResource(rImgs[position]);
         myTitle.setText(rTitle[position]);
--- app/src/main/java/com/example/pc2/lista.java
+++ app/src/main/java/com/example/pc2/lista.java
@@ -3,7 +3,11 @@ package com.example.pc2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+
+import Adapters.MyAdapter;
 
 public class lista extends AppCompatActivity {
 
@@ -11,7 +15,7 @@ public class lista extends AppCompatActivity {
     String mTitle[] = {"Los Rodríguez", "Santoro", "La Palomino", "Che carlitos", "Tradición arequipeña"};
     String mPedido[] ={"Pedido mínimo: S/.13","Pedido mínimo: S/.13", "Pedido mínimo: S/.16",
             "Pedido mínimo: S/.25", "Pedido mínimo: S/.15"};
-    String mEntrega[]={"Entrega: De 45 a 60 minutos", "Entrega: De 45 a 60 minutos", "Entrega: De 45 a 60 minutos"
+    String mEntrega[]={"Entrega: De 45 a 60 minutos", "Entrega: De 45 a 60 minutos", "Entrega: De 45 a 60 minutos",
             "Entrega: De 45 a 60 minutos", "Entrega: De 45 a 60 minutos"};
     int images[]= {R.drawable.i1, R.drawable.i2, R.drawable.i3, R.drawable.i4, R.drawable.i5};
 
@@ -19,5 +23,17 @@ public class lista extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista);
+        listView = findViewById(R.id.lv_courses);
+
+        MyAdapter adapter = new MyAdapter(this, mTitle, mPedido, mEntrega, images);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Sesiones(position, mTitle[position]);
+
+            }
+        });
     }
 }
